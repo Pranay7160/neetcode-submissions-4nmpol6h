549ff2a4ef6c0b8776de6b7c13ac0d8a8fb3f0d5
Pranay7160 <132940765+Pranay7160@users.noreply.github.com>
2026-08-30 09:37:28 +0530

    Add: sqrtx - submission-5

---
+++ Data Structures & Algorithms/sqrtx/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        res = 1
+        for i in range(1, (x//2)+1):
+            if i * i > x:
+                return res
+            res = i
+        
+        return res
+
+        # l = 1
+        # r = x // 2
+        # while l <= r:
+        #     m = (l+r)//2
+        #     mul_m = m * m
+        #     if mul_m == x:
+        #         return m
+        #     elif mul_m < x:
+        #         l = m+1
+        #     else:
+        #         r = m - 1
+        
+        # return l-1
+        
