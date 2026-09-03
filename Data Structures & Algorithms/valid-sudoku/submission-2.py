3fac08bd4db202d9242b5ee2cb3cbbc75aa9c794
Pranay7160 <132940765+Pranay7160@users.noreply.github.com>
2026-09-03 16:04:02 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_s = defaultdict(set)
+        col_s = defaultdict(set)
+        box = defaultdict(set) # tuple will be key
+
+        # this 2 loops will go to each element in matrix
+        for r in range(9): # 9 row
+            for c in range(9):
+                val = board[r][c] # acceing each value by row and col
+                if val == ".":
+                    continue
+
+                box_i = (r//3, c//3) # it wil become key for box dict
+                if val in row_s[r] or val in col_s[c] or val in box[box_i]:
+                    return False
+                
+                row_s[r].add(val)
+                col_s[c].add(val)
+                box[box_i].add(val)
+            
+        
+        return True
